fc49e8a21bd463ce2d8807dd586dc66416b4419b
nitaoktaviani28 <nitaoktaviani2005@gmail.com>
2026-02-26 20:50:27 +0700

    Update ObservabilityInit.cs

--- Observability/ObservabilityInit.cs
+++ Observability/ObservabilityInit.cs
@@ -1,21 +1,11 @@
-using Microsoft.Extensions.Logging;
-
 namespace Ecommerce.MonitoringApp.Observability;
 
 public static class ObservabilityInit
 {
     public static void Init(WebApplicationBuilder builder)
     {
-        var logger = LoggerFactory
-            .Create(b => b.AddConsole())
-            .CreateLogger("observability");
-
-        logger.LogInformation("Initializing observability...");
-
         Tracing.Setup(builder);
-        Metrics.Setup(builder);
+        AppMetrics.Setup(builder);
         Profiling.Setup();
-
-        logger.LogInformation("Observability initialized");
     }
 }
